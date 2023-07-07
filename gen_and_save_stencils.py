605f587260a74bf6f94b294b6c87a32ef8c46c69
Joshua-Miller-161 <jmill233@vols.utk.edu>
2023-07-07 15:17:18 -0400

    Switched from RBG-colors in shape arrays to integers which correspond to colors in the 'color_key' dictionary

--- gen_and_save_stencils.py
+++ gen_and_save_stencils.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import itertools
+from misc_utils import IntToColors
 from shapes import Rhombus, Square, Triangle, Circle, color_key
 from stacking_utils import StackStencils, StackTwoShapes, GetStencil
 #====================================================================
@@ -10,14 +13,39 @@ xmin = -20
 xmax = 20
 ymin = -20
 ymax = 20
+#====================================================================
+''' Choose stencils to make '''
+SHAPES = ['Circle', 'Square', 'Rhombus', 'Triangle']
+COLORS = [1, 2, 3]
+depth = 3 # Worked with 2
+#-------------------------------------------------------------------
+if (depth == 2):
+    COLORS = [1, 2]
+#-------------------------------------------------------------------
+shape_combs = list(itertools.permutations(SHAPES, depth))
+print("shape_combs =", shape_combs[2])
+#-------------------------------------------------------------------
+sizes = np.ones((len(shape_combs), depth), int)
 
-fig, ax = plt.subplots(1,1, figsize=(6,6))
+for i in range(np.shape(sizes)[0]):
+    for j in range(depth):
+        sizes[i][j] = int(np.random.randint(sizes[i][j-1 % depth], 19, 1))
+
+print("sizes =", sizes[10])
+#-------------------------------------------------------------------
+for i in range(len(shape_combs)):
+    layered, id = StackStencils(xmin, xmax, ymin, ymax, 
+                                shape_combs[i],
+                                sizes[i],
+                                COLORS)
+#====================================================================
+fig, ax = plt.subplots(1,1, figsize=(6, 6))
 
 #a, asd, asdw, sdfsdf = GetStencil(xmin, xmax, ymin, ymax, 'Circle', 2, 'hole', return_grid=True)
 
-x_1, y_1, x_grid, y_grid, shape_colors_1 = Rhombus(xmin, xmax, ymin, ymax, 15, 'blue')
-#x_2, y_2, x_grid, y_grid, shape_colors_2 = Square(xmin, xmax, ymin, ymax, 11, 'green')
-#x_3, y_3, x_grid, y_grid, shape_colors_3 = Triangle(xmin, xmax, ymin, ymax, 25, 'red')
+x_1, y_1, x_grid, y_grid, shape_colors_1 = Rhombus(xmin, xmax, ymin, ymax, 15, 2)
+#x_2, y_2, x_grid, y_grid, shape_colors_2 = Square(xmin, xmax, ymin, ymax, 11, 1)
+#x_3, y_3, x_grid, y_grid, shape_colors_3 = Triangle(xmin, xmax, ymin, ymax, 25, 3)
 #layered = StackTwoShapes(shape_colors_1, shape_colors_2)
 #ax.scatter(x=x_grid, 
 #           y=y_grid, 
@@ -25,14 +53,14 @@ x_1, y_1, x_grid, y_grid, shape_colors_1 = Rhombus(xmin, xmax, ymin, ymax, 15, '
 
 
 
-
+'''
 layered, id = StackStencils(xmin, xmax, ymin, ymax, 
                             ['Square', 'Rhombus', 'Square'],
                             [2, 17, 12],
                             ['blue', 'green', 'red'])
-
+'''
 ax.scatter(x=x_grid,
            y=y_grid,
-           c=layered)
+           c=IntToColors(layered, color_key))
 ax.set_title(id)
 plt.show()
